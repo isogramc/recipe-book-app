0f91b5724ebf128fe185987ec40578442ac6b1a3
theresajvrensburg <theresa@isogramconsulting.com>
2024-12-23 15:22:52 +0100

    tailwind css

--- recipe-book-app/src/components/RecipeCard.jsx
+++ recipe-book-app/src/components/RecipeCard.jsx
@@ -5,7 +5,7 @@ return (
         <div className="bg-white overflow-hidden">
             <img className="w-16 md:w-24 lg:w-38" src={image} alt={name}/>
         </div>
-        <div className="justify-self-start self-end">Recipe for {name}</div>
+        <div className="justify-self-start self-end">{name} Recipe</div>
         <div className="self-end">Calories: {calories}</div>
         <div className="self-end">Serves: {servings}</div>
     </div>
