46e53405842294a3481a1e33d25f26f4bad317b2
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2025-01-05 20:39:19 +0100

    Add Footer and Navbar

--- recipe-book-app/src/App.jsx
+++ recipe-book-app/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import { Routes, Route } from "react-router-dom"; 
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage"; 
-import ErrorPage from './pages/ErrorPage';
-import SideBar from './components/SideBar';
+import AboutPage from "./pages/AboutPage";
+import ErrorPage from "./pages/ErrorPage";
+import SideBar from "./components/SideBar";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <div>
-    <SideBar/>
-    <Routes>
-      <Route path="/" element={<HomePage />} /> 
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-   </div>
-  )
+    <div>
+      <Navbar />
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
