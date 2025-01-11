65dc1e0fc5f28cd19c68b844aaa6433c96d15f75
theresajvrensburg <theresa@isogramconsulting.com>
2025-01-11 13:45:44 +0100

    fixing project root

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -8,15 +8,14 @@ function RecipeCard(props){
     }
 
 return (
-    <div id={id} className="grid grid-cols-4 content-end">
+    <div id={id} className="grid grid-cols-5 content-end">
         <div className="bg-white overflow-hidden">
             <img className="w-16 md:w-24 lg:w-38" src={image} alt={name}/>
         </div>
         <div className="justify-self-start self-end">{name} Recipe</div>
         <div className="self-end">Calories: {calories}</div>
         <div className="self-end">Serves: {servings}</div>
-        <div><button onClick={deleteItem}>Delete</button></div>
-       
+        <div><button onClick={deleteItem}>Delete</button></div> 
     </div>
 )
 }
